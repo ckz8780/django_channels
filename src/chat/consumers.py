78e0aeb990e656b8dc9f691ea37dbe12062d83f1
Christopher Zielinski <ckz8780@gmail.com>
2018-07-16 15:04:45 -0400

    Added ChatConsumer

---
+++ src/chat/consumers.py
@@ -0,0 +1,20 @@
+import asyncio
+import json
+from django.contrib.auth import get_user_model
+from channels.consumer import AsyncConsumer
+from channels.db import database_sync_to_async
+
+from .models import Thread, ChatMessage
+
+class ChatConsumer(AsyncConsumer):
+    async def websocket_connect(self, event):
+        # When the socket connects
+        print('Connected', event)
+
+    async def websocket_receive(self, event):
+        # When a message is received from the websocket
+        print('Received', event)
+
+    async def websocket_disconnect(self, event):
+        # When the socket disconnects
+        print('Disconnected', event)
