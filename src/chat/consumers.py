29de9a0f950b3c640827b5a3d6abf2f0ccc8eb57
Christopher Zielinski <ckz8780@gmail.com>
2018-07-16 15:48:24 -0400

    Updated ChatConsumer connect method

--- src/chat/consumers.py
+++ src/chat/consumers.py
@@ -10,6 +10,13 @@ class ChatConsumer(AsyncConsumer):
     async def websocket_connect(self, event):
         # When the socket connects
         print('Connected', event)
+        await self.send({
+            'type': 'websocket.accept'
+        })
+        await self.send({
+            'type': 'websocket.send',
+            'text':'Hello World!'
+        })
 
     async def websocket_receive(self, event):
         # When a message is received from the websocket
